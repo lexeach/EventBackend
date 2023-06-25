1daee7b07ae3fafa4292d7f65ce5cb9cd7749787
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2023-06-25 12:26:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -9,53 +10,34 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+
+
+
+app.get('/get-price', async (req, res) => {
+  const apiKey = 'bc093fe3-1948-4939-8638-2c01e69a1afa';
+  const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?convert_id=2796&id=24638`;
+
+  const headers = {
+    Accepts: 'application/json',
+    'X-CMC_PRO_API_KEY': apiKey,
+  };
+
+  try {
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+
+    if (data.data) {
+      const nrkInrData = Math.round(data.data[24638].quote[2796].price * 100);
+      res.json({ data: nrkInrData });
+    } else {
+      res.json({ data: 778 });
+    }
+  } catch (error) {
+    res.json({ data: 778 });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
